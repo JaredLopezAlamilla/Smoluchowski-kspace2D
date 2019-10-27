249f1e0bb5360c9bf3548f07e9c42294d84b7336
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-28 12:17:11 +1300

    Update IterativeOptimyzed2D.m

--- IterativeOptimyzed2D.m
+++ IterativeOptimyzed2D.m
@@ -6,4 +6,4 @@ fileID = fopen('EvolutionIterations1.txt','at');
 fprintf(fileID, '%s\t\t%s\t\t%s\t\t%s\t\t%s\n','MSEpot','fx','Fx','fy','Fy');
 fclose(fileID);
 
-[Fs,fval,exitflag,output]= fsolve(@(F) abs(F-iterativo2D(F,P_pos,V2D,Jxkq,Jykq,Ns,MSEs)),[0;0],options); % 2D case
+[Fs,fval,exitflag,output]= fsolve(@(F) abs(F-iterativo2D(F,P_pos,V2D,Jxkq,Jykq,L,Ns,MSEs)),[0;0],options); % 2D case
